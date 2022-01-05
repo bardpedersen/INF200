00103627124db2d2d40e2f00492079fc58c7a97a
Bård Pedersen <bard.tollef.pedersen@nmbu.no>
2022-01-05 13:51:50 +0100

    Made island map with corosponding landscapes

--- biosim_template/src/biosim/island_map.py
+++ biosim_template/src/biosim/island_map.py
@@ -1,68 +1,40 @@
-]    def feeding(self):
-        """
-        Animals residing in a cell eat in descend- ing order of fitness.
-        Each animal tries every year to eat an amount F of fodder,
-        but how much feed the animal obtain depends on fodder available in the cell
+"""
+Different type of landscapes
+Geogrofi
+Migration
+location
+"""
+import numpy as np
+from .landscapes import LowLand, Water
+from .animals import Herbivore
 
-        Here:
-        F = 10
-        f_max = 800
-        """
-        self.animals_in_cell.sort(reversed=True, key='fitness')
-        all_herbivore = np.array(self.animals_in_cell)
-        available_fodder = 800
-        appetite = 10
-        for herbivore in all_herbivore:
-            if available_fodder == 0:
-                break
-            elif available_fodder >= appetite:
-                # herbivore.eat(appetite)
-                available_fodder = available_fodder - appetite
-            elif 0 < available_fodder < appetite:
-                # herbivore.eat(available_fodder)
-                available_fodder = 0
+class Map:
+    def __init__(self, island_map):
+        self._map = island_map  #Information we get from mono_ho
+        self._landcape = {'W': Water,
+                          'L': LowLand}
 
-    def procreation(self):
+        self.cells = self.creating_island #Individual cells that make up the map.
+
+    def creating_island(self):
         """
-        Animals can mate if there are at least two animals of the same species in a cell.
-        Gender plays no role in mating.Each animal can give birth to at most one off- spring per year.
-        At birth, the mother animal loses ξ times the actual birth weight of the baby.
-        If the mother would lose more than her own weight,
-        then no baby is born and the weight of the mother remains unchanged.
+        first str to matirx
         """
-        pass
+        map_str_clean = self._map.replace(' ', '')  #Removes empty lines
+        matrix_map = np.array([[j for j in i] for i in map_str_clean.splitlines()]) #Splits the str to array/matrix
+        """
+        Turn each symbols to right landscape
+        """
+        cells_with_land = np.empty(self.matrix_map.shape, dtype=object)
+        for i, k in enumerate(matrix_map):
+            for j, _letter in enumerate(k): #i and j is the cordinates, letter is the landscape of the cordinates
+                cells_with_land[i][j] = self._landcape[_letter]
+
+        return cells_with_land
+
 
     def migration(self):
         """No migration on one cell island"""
         pass
 
-    def aging(self):
-        """
-        At birth, each animal has age a = 0 .
-        Age increases by one year for each year that passes
-        """
-        for animal in self.animals_in_cell:
-            # animal.grow_up()
-            pass
 
-    def weight(self):
-        """
-        Every year, the weight of the animal decreases.
-        When an animal eats an amount of fodder, its weight increases.
-        """
-        for animal in self.animals_in_cell:
-            # animal.lose_weight
-            pass
-
-        for animal in self.animals_in_cell:
-            # animal.gain_weight
-            pass
-
-    def death(self):
-        """
-        Animals die when its weight is w = 0
-        """
-        for animal in self.animals_in_cell:
-            if animal.death:
-                # self.animals_in_cell.remove(animal)
-                pass
