e3374bba48ef87738fdb441f75e16b4e4a81618d
Bård Pedersen <bard.tollef.pedersen@nmbu.no>
2022-01-07 14:18:20 +0100

    Added lists to plot

--- biosim_template/src/biosim/visualization.py
+++ biosim_template/src/biosim/visualization.py
@@ -1,4 +1,4 @@
-from biosim.animals import Herbivore
+from biosim.animals import Herbivore, Carnivore
 from biosim.landscapes import LowLand, Water
 from biosim.island_map import Map
 
@@ -8,18 +8,41 @@ import matplotlib.pyplot as plt
 
 class WindowPlot:
     def __init__(self, island_map):
-        self.x_list = []
-        self.y_list = []
+        self.nr_herb = []
+        self.nr_car = []
+        self.weight_herb = []
+        self.weight_car = []
+        self.fitness_herb = []
+        self.fitness_car = []
+        self.age_herb = []
+        self.age_car = []
+        self.years = []
         self.island_map = island_map
 
-    def update_graph_x(self, year):
-        self.x_list.append(year)
+    def update_years(self, year):
+        self.years.append(year)
 
-    def update_graph_y(self, num_animals):
-        self.y_list.append(num_animals)
+    def update_animals(self, num_animals_herb, num_animals_car):
+        self.nr_herb.append(num_animals_herb)
+        self.nr_car.append(num_animals_car)
+
+    def update_animals_age(self):
+        self.age_car.append()
+        self.age_herb.append()
+        pass
+
+    def update_animals_fitness(self):
+        self.fitness_car.append()
+        self.fitness_herb.append()
+        pass
+
+    def update_animals_weight(self):
+        self.weight_car.append()
+        self.weight_herb.append()
+        pass
 
     def make_graph(self):
-        plt.plot(self.x_list, self.y_list)
+        plt.plot(self.years, self.nr_herb)
         plt.xlabel('Years')
         plt.ylabel('Animals')
         plt.title('Herbivores in single cell')
@@ -34,7 +57,7 @@ class WindowPlot:
                       for row in self.island_map.splitlines()]
 
         fig = plt.figure()
-        image_axes = fig.add_axes([0.1, 0.1, 0.7, 0.8])#(start point, x), (start point y),
+        image_axes = fig.add_axes([0.1, 0.1, 0.7, 0.8])  # (start point, x), (start point y),
         image_axes.imshow(colour_map)
 
     def one_graph(self):
